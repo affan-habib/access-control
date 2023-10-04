55ce5d3295d5a8e18d16505fdb9dd6dea5530d61
affan-habib <affan.habib44@gmail.com>
2023-10-04 18:06:29 +0600

    menu and submenu

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,63 +1,38 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Button, Menu, MenuItem, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 import menuData from 'menu-items';
+import React from 'react';
 
 
-function MenuBar() {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [selectedMenuItem, setSelectedMenuItem] = useState(null);
-    const open = Boolean(anchorEl);
-
-    const handleMenuClick = (event, index) => {
-        setAnchorEl(event.currentTarget);
-        setSelectedMenuItem(index);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
-
+const renderMenu = (items) => {
     return (
-        <div>
-            <AppBar position="static" sx={{ boxShadow: 0 }}>
-                <Toolbar sx={{ bgcolor: ' #333' }}>
-                    {menuData.map((menuItem, index) => (
-                        <div key={index}>
-                            {menuItem.submenu ? (
-                                <Button
-                                    color={selectedMenuItem === index ? 'info' : 'inherit'}
-                                    onClick={(event) => handleMenuClick(event, index)}
-                                >
-                                    {menuItem.label}
-                                </Button>
-                            ) : (
-                                <Button color="inherit" href={menuItem.link} disabled={menuItem.disabled}>
-                                    {menuItem.label}
-                                </Button>
-                            )}
-                            {menuItem.submenu && (
-                                <Menu
-                                    anchorEl={anchorEl}
-                                    open={open}
-                                    onClose={handleMenuClose}
-                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-                                    transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-                                    getContentAnchorEl={null}
-                                >
-                                    {menuItem.submenu.map((subItem, subIndex) => (
-                                        <MenuItem key={subIndex} onClick={handleMenuClose}>
-                                            {subItem.label}
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            )}
-                        </div>
-                    ))}
-                </Toolbar>
-            </AppBar>
-        </div>
+        <ul className="menu-list">
+            {items.map((item) => (
+                <li className="menu-item" key={item.label}>
+                    {item.label}
+                    {item.submenu && renderSubmenu(item.submenu)}
+                </li>
+            ))}
+        </ul>
     );
-}
+};
+
+const renderSubmenu = (submenuItems) => {
+    return (
+        <ul className="submenu">
+            {submenuItems.map((submenuItem) => (
+                <li className="submenu-item" key={submenuItem.label}>
+                    {submenuItem.label}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const MenuBar = () => {
+    return (
+        <nav className="menu-bar">
+            {renderMenu(menuData)}
+        </nav>
+    );
+};
 
 export default MenuBar;
--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -2,44 +2,165 @@ import HomeIcon from '@mui/icons-material/Home';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 
 const menuData = [
-  {
-    label: 'Dropdown',
-    submenu: [
-      {
-        label: 'Action',
-        link: '/ss',
-      },
-      {
-        label: 'Another action',
-        link: '#',
-      },
-      {
-        label: 'Something else here',
-        link: '#',
-      },
-    ],
-  },
-  {
-    label: 'Dropdown',
-    submenu: [
-      {
-        label: 'sssss',
-        link: '/ss',
-      },
-      {
-        label: 'Another actionsssssssssssssaffffffffffffffff',
-        link: '#',
-      },
-      {
-        label: 'Something else heresaeeeeeeeeeeeeeeeeeeee',
-        link: '#',
-      },
-    ],
-  },
-  {
-    label: 'Disabled',
-    link: '#',
-    disabled: true,
-  },
+    {
+        label: 'MDs Office',
+        submenu: [
+            {
+                label: 'User Setup Entry',
+                link: '/ss',
+            },
+            {
+                label: 'Access Control Entry',
+                link: '#',
+            },
+            {
+                label: 'MBL Setup Entry',
+                link: '#',
+            },
+            {
+                label: 'Unit Ownership Entry',
+                link: '#',
+            },
+            {
+                label: 'MBLP Authority Delegation Entry',
+                link: '#',
+            },
+            {
+                label: 'MBLP Plan Entry',
+                link: '#',
+            },
+            {
+                label: 'Cash Flow Projection Entry',
+                link: '#',
+            },
+            {
+                label: 'Project cancel Entry',
+                link: '#',
+            },
+            {
+                label: 'MBL Setup View',
+                link: '#',
+            },
+            {
+                label: 'Unit Ownwership',
+                link: '#',
+            },
+            {
+                label: 'MBL Plan',
+                link: '#',
+            },
+            {
+                label: 'Cashflow',
+                link: '#',
+            },
+        ],
+    },
+    {
+        label: 'Land',
+        submenu: [
+            {
+                label: 'Land Profile Entry',
+                link: '/#',
+            },
+            {
+                label: 'Lo Profile Entry',
+                link: '#',
+            },
+            {
+                label: 'SignM Pay Schedule Entry',
+                link: '#',
+            },
+            {
+                label: 'SignM Pay Entry',
+                link: '#',
+            },
+            {
+                label: 'Commitment to LO Entry',
+                link: '#',
+            },
+            {
+                label: 'land Register',
+                link: '#',
+            },
+            {
+                label: 'LO Register',
+                link: '#',
+            },
+            {
+                label: 'SignM Pay Schedule',
+                link: '#',
+            },
+            {
+                label: 'SignM Pay Register',
+                link: '#',
+            },
+            {
+                label: 'SignM Pay Assisttant *',
+                link: '#',
+            },
+            {
+                label: 'Commitment to LO',
+                link: '#',
+            },
+        ],
+    },
+    {
+        label: 'Marketing',
+        submenu: [
+            {
+                label: 'Booking Form Entry',
+                link: '/#',
+            },
+            {
+                label: 'Buyer Profile Entry',
+                link: '#',
+            },
+            {
+                label: 'Sales Chart Entry',
+                link: '#',
+            },
+            {
+                label: 'Price Chart Entry',
+                link: '#',
+            },
+            {
+                label: 'Sales Offer Entry',
+                link: '#',
+            },
+            {
+                label: 'Booking Form',
+                link: '#',
+            },
+            {
+                label: 'Buyer Register',
+                link: '#',
+            },
+            {
+                label: 'Sales Status',
+                link: '#',
+            },
+            {
+                label: 'Sales Chart',
+                link: '#',
+            },
+            {
+                label: 'Price Chart',
+                link: '#',
+            },
+            {
+                label: 'Availability Chart',
+                link: '#',
+            },
+            {
+                label: 'Sales Offer Register',
+                link: '#',
+            },
+            {
+                label: 'Lead Generation Entry',
+                link: '#',
+            },
+        ],
+    },
+
 ];
 export default menuData;
